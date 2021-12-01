13b1d167aaaf45d941936d63388a52f387792536
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-01 18:15:45 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
+                + "\nUploaded to YouTube. Trusted Thunder Team"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
